72c9f6aaf55071f83419b34e2490e55da9bd618b
abid413119 <abidastronaut@gmail.com>
2020-03-27 13:11:42 +0600

    create post desing v2

--- src/Components/CreatePost/CreatePost.js
+++ src/Components/CreatePost/CreatePost.js
@@ -2,15 +2,29 @@ import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { TextField, InputAdornment, Typography } from '@material-ui/core';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import { Card, CardActions, IconButton, Button } from '@material-ui/core';
+import { Card, CardActions, CardMedia, IconButton, Button, TextareaAutosize } from '@material-ui/core';
 import PersonIcon from '@material-ui/icons/Person';
 import RoomIcon from '@material-ui/icons/Room';
+import Avatar from '@material-ui/core/Avatar';
+import Av1 from '../../assets/image/avatar1.jpg'
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
     root: {
         alignItems: "flex-end"
     },
+    TextareaAutosize: {
+        width: "100%",
+        paddingLeft: "60px",
+        paddingTop: "10px",
+        outline: "none",
+        border: "none",
+        borderBottom: "1px solid #ccc"
+    },
+    Avater: {
+        position: "absolute",
+        margin: "5px"
+    }
     
 }));
 
@@ -20,17 +34,15 @@ function CreatePost() {
     return (
         <>
             <Card>
-                <TextField
-                    id="input-with-icon-textfield"
-                    rows="5"
-                    multiline
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="end" className={classes.root}>
-                                <AccountCircle />
-                            </InputAdornment>
-                        ),
-                    }} />
+                <CardMedia>
+                    <Avatar className={classes.Avater} alt="Remy Shar" src={Av1} />
+                </CardMedia>
+                <TextareaAutosize
+                    className={classes.TextareaAutosize}
+                    aria-label="empty textarea" 
+                    placeholder="What's on your mind?" 
+                    rowsMin={6}
+                />
 
                 <CardActions style={{justifyContent: "space-between"}}>
                     <div>
@@ -44,7 +56,7 @@ function CreatePost() {
                         <Typography>Check In</Typography>
                     </IconButton>
                     </div>
-                    <Button variant="contained" size="large" color="primary" className={classes.shareButton}>
+                    <Button variant="contained" size="medium" color="primary" className={classes.shareButton}>
                         Share
                     </Button>
                 </CardActions>
--- src/theme/overrides/MuiIconButton.js
+++ src/theme/overrides/MuiIconButton.js
@@ -3,6 +3,8 @@ import palette from '../palette';
 export default {
     root: {
         color: palette.icon,
+        borderRadius: "6px",
+        padding: 5,
         '&:hover': {
             backgroundColor: 'rgba(0, 0, 0, 0.03)'
         }
