981916b2987e754b0d57a99e6103b432a05952df
vcshahriyer <shahriyer99@gmail.com>
2020-03-28 11:00:24 +0600

    merge conflict resolved

--- src/Components/CreatePost/CreatePost.js
+++ src/Components/CreatePost/CreatePost.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { makeStyles } from '@material-ui/styles';
-import { Typography } from '@material-ui/core';
-import { Card, CardActions, CardMedia, IconButton, Button, TextareaAutosize } from '@material-ui/core';
-import PersonIcon from '@material-ui/icons/Person';
-import RoomIcon from '@material-ui/icons/Room';
-import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
-import Avatar from '@material-ui/core/Avatar';
-import Av1 from '../../assets/image/avatar1.jpg'
-
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        alignItems: "flex-end"
-    },
-    TextareaAutosize: {
-        width: "100%",
-        padding: "1rem 1rem 1rem 4rem",
-        outline: "none",
-        border: "none",
-        borderBottom: "1px solid #ccc"
-    },
-    Avater: {
-        position: "absolute",
-        margin: ".5rem"
-    }
-    
-=======
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
 import { Typography } from "@material-ui/core";
@@ -59,7 +30,6 @@ const useStyles = makeStyles(theme => ({
     position: "absolute",
     margin: "5px"
   }
->>>>>>> 822a1f8d25e93e02b0ce072a48afbf1818977cf0
 }));
 
 function CreatePost() {
@@ -85,26 +55,6 @@ function CreatePost() {
               <Typography>People</Typography>
             </IconButton>
 
-<<<<<<< HEAD
-                    <IconButton>
-                        <RoomIcon />
-                        <Typography>Check In</Typography>
-                    </IconButton>
-
-                    <IconButton>
-                        <EmojiEmotionsIcon />
-                        <Typography>Mood</Typography>
-                    </IconButton>
-                    </div>
-                    <Button variant="contained" size="medium" color="primary" className={classes.shareButton}>
-                        Share
-                    </Button>
-                </CardActions>
-
-            </Card>
-        </>
-    )
-=======
             <IconButton>
               <RoomIcon />
               <Typography>Check In</Typography>
@@ -122,6 +72,5 @@ function CreatePost() {
       </Card>
     </>
   );
->>>>>>> 822a1f8d25e93e02b0ce072a48afbf1818977cf0
 }
 export default CreatePost;
